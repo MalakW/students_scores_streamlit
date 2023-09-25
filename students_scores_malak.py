1dd072d2a9492ef55c9d2ec504a0023d21967f7c
Malak Wehbe <51320105+MalakW@users.noreply.github.com>
2023-09-25 17:38:31 +0300

    Update students_scores_malak.py

--- students_scores_malak.py
+++ students_scores_malak.py
@@ -5,6 +5,7 @@ import pandas as pd
 import numpy as np 
 import plotly.express as px  
 import plotly.graph_objects as go
+import streamlit.components.v1 as components
 
 #setting wide screen page for streamlite
 st.set_page_config(layout = 'wide')
